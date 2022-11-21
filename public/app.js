e86300c194ef837b2fed55c1376261b80f50e01a
pmeuwissen <pmeuwissen@example.com>
2022-11-20 23:19:14 -0600

    database code

--- public/app.js
+++ public/app.js
@@ -1,30 +1,61 @@
-const loginBackground = document.getElementById('login-background');
-const loginWindow = document.getElementById('login-window');
-const signupWindow = document.getElementById('signup-window');
-document.getElementById("login").onclick = function() {
+const loginBackground = document.getElementById("login-background");
+const loginWindow = document.getElementById("login-window");
+const signupWindow = document.getElementById("signup-window");
+document.getElementById("login").onclick = function () {
   loginBackground.style.display = "block";
   loginWindow.style.display = "block";
-  setTimeout(function(){
-    loginBackground.style.opacity="1";
-    loginWindow.style.opacity="1"
+  setTimeout(function () {
+    loginBackground.style.opacity = "1";
+    loginWindow.style.opacity = "1";
   }, 50);
-}
-document.getElementById("signup").onclick = function() {
+};
+document.getElementById("signup").onclick = function () {
   loginBackground.style.display = "block";
   signupWindow.style.display = "block";
-  setTimeout(function(){
-    loginBackground.style.opacity="1";
-    signupWindow.style.opacity="1"
+  setTimeout(function () {
+    loginBackground.style.opacity = "1";
+    signupWindow.style.opacity = "1";
   }, 50);
-}
+};
 
-loginBackground.onclick = function() {
+loginBackground.onclick = function () {
   loginBackground.style.opacity = "0";
   loginWindow.style.opacity = "0";
   signupWindow.style.opacity = "0";
-  setTimeout(function(){
+  setTimeout(function () {
     loginBackground.style.display = "none";
     loginWindow.style.display = "none";
     signupWindow.style.display = "none";
   }, 200);
-}
+};
+
+// let space1 = {
+//   name: "Spaces Capitol East District",
+//   address:
+//     "811 East Washington Avenue, 3rd and 4th Floor, Madison, Wisconsin 53703, United States of America",
+//   hours: {
+//     monday: "8:30-5:00",
+//     tuesday: "8:30-5:00",
+//     wednesday: "8:30-5:00",
+//     thursday: "8:30-5:00",
+//     friday: "8:30-5:00",
+//   },
+//   company_email: null,
+//   phone: 18884254853,
+//   description:
+//     "Sharing tenancy with a brewery and a live music venue, this business centre sums of Madison perfectly: working hard and playing hard too. Spaces Capital East District is a distinctive cube building that never fails to make a great impression. From the branded elevator to the designer decor and a private rooftop terrace, this is an inspriational, flexible workspace that's hard to match. Stay productive with unlimited Wi-Fi, friendly staff and motivating, creative vibe. ",
+//   website:
+//     "https://www.spacesworks.com/madison/capitol-east-district/?utm_source=yext_places_gmb&utm_medium=places&utm_campaign=yext_traffic&utm_content=4980",
+//   capacity: "55 offices",
+//   private_office: true,
+//   meeting_rooms: true,
+//   parking: true,
+//   events: true,
+//   price_range: "55-239",
+// };
+
+// let space0 = {
+//   name: "test",
+// };
+
+// db.collection("coworkingSpaces").add(space0);
